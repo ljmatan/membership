349ca2b0d959a564f56771b942312e373d6559e7
user <lj.matan@gmail.com>
2021-03-23 02:11:44 +0100

    tidying up

--- lib/ui/view/main_view.dart
+++ lib/ui/view/main_view.dart
@@ -28,19 +28,95 @@ class _MainViewState extends State<MainView> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: PageView(
-            controller: _pageController,
-            physics: const NeverScrollableScrollPhysics(),
-            children: [
-              HomePage(),
-            ],
+    return Scaffold(
+      backgroundColor: const Color(0xffD7D7D7),
+      appBar: AppBar(
+        elevation: 4,
+        titleSpacing: 0,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 12),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xffE4E4E4),
+                      prefixIcon: Icon(Icons.search),
+                      hintText: 'Cari produk harga terbaik',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      contentPadding: const EdgeInsets.all(0),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Icon(
+                      Icons.center_focus_strong,
+                      color: const Color(0xff2B6182),
+                    ),
+                  ),
+                  onTap: () => null,
+                ),
+                GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  child: Icon(
+                    Icons.forum,
+                    color: const Color(0xffED4242),
+                  ),
+                  onTap: () => null,
+                ),
+                GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Icon(
+                      Icons.notifications,
+                      color: const Color(0xff999999),
+                    ),
+                  ),
+                  onTap: () => null,
+                ),
+              ],
+            ),
           ),
         ),
-        CustomNavBar(),
-      ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              physics: const NeverScrollableScrollPhysics(),
+              children: [
+                HomePage(),
+              ],
+            ),
+          ),
+          CustomNavBar(),
+        ],
+      ),
     );
   }
 
